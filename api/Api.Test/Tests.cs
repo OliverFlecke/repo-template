1a0ac309f27b7504115dd443c3af42f14c4d149f
Oliver Fleckenstein <oliver@flecke.xyz>
2026-07-27 11:15:20 +0800

    build: integration test project for api

---
+++ api/Api.Test/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Api.Test;
+
+public sealed class HealthCheckTest
+{
+	[ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
+	public required WebApplicationFactory WebApplicationFactory { get; init; }
+
+	[Test]
+	public async Task HealthCheck_RespondWithOk200()
+	{
+		var client = WebApplicationFactory.CreateClient();
+
+		var response = await client.GetAsync("/healthz");
+
+		await Assert.That(response).HasStatusCode(HttpStatusCode.OK);
+	}
+}
---
+++ api/Api.Test/WebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TUnit.Core.Interfaces;
+
+namespace Api.Test;
+
+public class WebApplicationFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+	public Task InitializeAsync()
+	{
+		_ = Server;
+
+		return Task.CompletedTask;
+	}
+}
