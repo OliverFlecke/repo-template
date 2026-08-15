953eb869c722d91cf8dce4cec79df3d2c08e4e25
Oliver Fleckenstein <oliver@flecke.xyz>
2026-08-15 20:53:45 +0800

    chore: remove sample, as it was causing too much noise

--- flare/sample/mip/prod_00/nvflare_compose_client/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-: "${NVFLARE_CLIENT_NAME:?NVFLARE_CLIENT_NAME must be set}"
-: "${NVFLARE_ORG:?NVFLARE_ORG must be set}"
-exec python3 -u -m nvflare.private.fed.app.client.client_train \
-	-m /workspace -s fed_client.json --set secure_train=true \
-	"uid=${NVFLARE_CLIENT_NAME}" "org=${NVFLARE_ORG}" config_folder=config
--- flare/sample/mip/prod_00/nvflare_compose_server/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-: "${NVFLARE_ORG:?NVFLARE_ORG must be set}"
-exec python3 -u -m nvflare.private.fed.app.server.server_train \
-	-m /workspace -s fed_server.json --set secure_train=true \
-	config_folder=config "org=${NVFLARE_ORG}"
